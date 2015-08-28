2ab2398164153c1bcb2562d0fe223fa88a0d58ef
Brit Butler <redline6561@gmail.com>
2015-08-27 23:40:17 -0400

    Scaffold out routes.

--- lib/lita/handlers/help_queue.rb
+++ lib/lita/handlers/help_queue.rb
@@ -1,7 +1,37 @@
 module Lita
   module Handlers
     class HelpQueue < Handler
-      # insert handler code here
+      route(/^halp me$/, :add, command: true, help: { })
+      route(/^halp nvm$/, :cancel, command: true, help: { })
+      route(/^halp queue$/, :show, command: true, help: { })
+      route(/^halp count$/, :count, command: true, help: { })
+      route(/^halp next$/, :next, command: true, restrict_to: [:instructors],
+        help: { })
+      route(/^halp drop\s+(.+)$/, :drop, command: true, restrict_to: [:instructors],
+        help: { })
+      route(/^halp clear$/, :clear, command: true, restrict_to: [:instructors],
+        help: { })
+
+      def add(response)
+      end
+
+      def cancel(response)
+      end
+
+      def show(response)
+      end
+
+      def count(response)
+      end
+
+      def next(response)
+      end
+
+      def drop(response)
+      end
+
+      def clear(response)
+      end
 
       Lita.register_handler(self)
     end
