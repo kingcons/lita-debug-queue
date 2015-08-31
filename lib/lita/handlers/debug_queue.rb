090d806bed73b0ff3dc8cff8e2e2b45ac8deba61
Brit Butler <redline6561@gmail.com>
2015-08-31 15:22:13 -0500

    Minor style tweaks.

--- lib/lita/handlers/debug_queue.rb
+++ lib/lita/handlers/debug_queue.rb
@@ -3,16 +3,20 @@ module Lita
     class DebugQueue < Handler
       config :classrooms # A mapping from instructor names to classroom channels.
 
-      route(/^debug me$/, :add, command: true, help: { })
-      route(/^debug nvm$/, :cancel, command: true, help: { })
-      route(/^debug queue$/, :show, command: true, help: { })
-      route(/^debug count$/, :count, command: true, help: { })
+      route(/^debug me$/, :add, command: true,
+        help: { 'debug me' => 'Put your name in the queue for debugging help.' })
+      route(/^debug nvm$/, :cancel, command: true,
+        help: { 'debug nvm' => 'Remove your name from the queue for debugging help.' })
+      route(/^debug queue$/, :show, command: true,
+        help: { 'debug queue' => 'Show the current queue for your class.' })
+      route(/^debug count$/, :count, command: true,
+        help: { 'debug count' => 'Count the number of people waiting for help.' })
       route(/^debug next$/, :next, command: true, restrict_to: [:instructors],
-        help: { })
+        help: { 'debug next' => 'Notify the next student to be helped.' })
       route(/^debug drop\s+(.+)$/, :drop, command: true, restrict_to: [:instructors],
-        help: { })
+        help: { 'debug drop NAME' => 'Remove the student with NAME from the queue.' })
       route(/^debug clear$/, :clear, command: true, restrict_to: [:instructors],
-        help: { })
+        help: { 'debug clear' => 'Empty the queue.' })
 
       def add(response)
         return unless check_room!(response)
@@ -29,7 +33,7 @@ module Lita
         return unless check_room!(response)
         student = response.user.mention_name
         if room_queue.include?(student)
-          redis.set(@room, room_queue.reject{ |x| x == student })
+          redis.set(@room, room_queue.reject { |x| x == student })
           response.reply("#{student}: Glad you figured it out! :)")
         else
           response.reply("#{student}: You know you're not in the queue, right?")
@@ -77,8 +81,8 @@ module Lita
       end
 
       def room_queue
-        room = redis.get(@room)
-        room ? MultiJson.load(room) : []
+        data = redis.get(@room)
+        data ? MultiJson.load(data) : []
       end
     end
 
